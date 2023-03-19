12bcf7c6913d21961bda50354b88a280967c0ab7
OskarsOrinskis <125825846+OskarsOrinskis@users.noreply.github.com>
2023-03-19 16:21:14 +0000

    aabc

--- build_heap.py
+++ build_heap.py
@@ -11,25 +11,18 @@ def build_heap(data):
     for i in range(n // 2, -1, -1):
 
         min_index = i
-
         l = 2 * i + 1
 
         if l < n and data[l] < data[min_index]:
-
             min_index = 1
-
-        r = 2 * i +2
+        r = 2 * i + 2
 
         if r < n and data[r] < data[min_index]:
-
             min_index = r
 
         if i != min_index:
-
             swaps.append((i, min_index))
-
             data[i], data[min_index] = data[min_index], data[i]
-
             swaps += build_heap(data[min_index:])
 
     return swaps
@@ -61,8 +54,6 @@ def main():
     
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
-
-
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
