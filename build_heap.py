f30ffe4d02d747f8d43dc5c1abcf6d0af6c7743d
OskarsOrinskis <125825846+OskarsOrinskis@users.noreply.github.com>
2023-03-19 16:27:56 +0000

    cc

--- build_heap.py
+++ build_heap.py
@@ -30,29 +30,22 @@ def build_heap(data):
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+
 
     input_str = input().strip()
 
     input_str = input_str.replace('I', '').replace('F', '')
 
-    # input from keyboard
     n = int(input_str)
     
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
-   swaps = build_heap(data)
+    swaps = build_heap(data)
 
 
     
-    # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
     # output all swaps
     print(len(swaps))
