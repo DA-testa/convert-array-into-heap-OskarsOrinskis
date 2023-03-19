2e89ea42f721734fe8aecff782ff8cf8d2f45ba0
OskarsOrinskis <125825846+OskarsOrinskis@users.noreply.github.com>
2023-03-19 15:26:11 +0000

    a

--- build_heap.py
+++ build_heap.py
@@ -6,6 +6,31 @@ def build_heap(data):
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
+    n = len(data)
+
+    for i in range(n // 2, -1, -1):
+
+        min_index = i
+
+        l = 2 * i + 1
+
+        if l < n and data[l] < data[min_index]:
+
+            min_index = 1
+
+        r = 2 * i +2
+
+        if r < n and data[r] < data[min_index]:
+
+            min_index = r
+
+        if i != min_index:
+
+            swaps.append((i, min_index))
+
+            data[i], data[min_index] = data[min_index], data[i]
+
+            swaps += build_heap(data[min_index])
 
     return swaps
 
