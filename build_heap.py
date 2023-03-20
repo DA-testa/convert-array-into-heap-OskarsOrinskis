d77a0e9822aba083a311e7bd6df237c6b85fb384
OskarsOrinskis <125825846+OskarsOrinskis@users.noreply.github.com>
2023-03-20 01:04:04 +0000

    oo

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,16 @@
 # python3
 import queue
 def build_heap(n, j, data):
-
     treads = queue.PriorityQueue()
-
     for i in range(n):
-
         treads.put((0, i))
-
     swaps = []
 
     for i in range(j):
-        
         data_time = data[i]
-    
         start_time, tread = treads.get()
-
         swaps.append((tread, start_time))
-
         treads.put((sart_time + data_time, tread))
-
     return swaps
 
    
@@ -31,11 +22,11 @@ def build_heap(n, j, data):
 def main():
     try:
         n, j = map(int, input().split())
+        data = list(map(int, input().split()))
     except ValueErorr:
         return
-    data = list(map(int, input().split()))
-    x = build_heap(n, j, data)
     
+    x = build_heap(n, j, data)
     for i in range(j):
         print(x[i][0], x[i][1])
 
